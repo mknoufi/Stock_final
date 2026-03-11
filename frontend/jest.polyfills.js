66e2a9c118d37c655866e024b91d54ad9d23a4e1
Noufi <mknoufi@gmail.com>
2026-03-11 12:18:38 +0530

    Fix Jest env to use NODE_ENV=test for RN act support

--- frontend/jest.polyfills.js
+++ frontend/jest.polyfills.js
@@ -1,3 +1,6 @@
+// Ensure React/Jest load test builds even if shell exports NODE_ENV=production
+process.env.NODE_ENV = "test";
+
 // Fix for "The global process.env.EXPO_OS is not defined" warning
 if (!process.env.EXPO_OS) {
   process.env.EXPO_OS = "ios";
